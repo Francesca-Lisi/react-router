4199a72050392fd21b4f03079a5c1bd52257805b
Francesca-Lisi <francescalisi2143@gmail.com>
2025-01-23 17:29:25 +0100

    feat: add structure in App.jsx and MainMenu.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
+import PostPages from "./pages/PostPages"
+
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/chi-siamo" Component={AboutPage} />
+          <Route path="/articoli" Component={PostPages} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
